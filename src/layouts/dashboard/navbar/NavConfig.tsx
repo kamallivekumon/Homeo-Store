8388844c6fb38da30e9d384efac3ee1068ff6648
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2022-05-20 21:50:25 +0530

    Initial

---
+++ src/layouts/dashboard/navbar/NavConfig.tsx
@@ -0,0 +1,68 @@
+// routes
+import { PATH_DASHBOARD } from '../../../routes/paths';
+// components
+import Label from '../../../components/Label';
+import Iconify from '../../../components/Iconify';
+import SvgIconStyle from '../../../components/SvgIconStyle';
+
+// ----------------------------------------------------------------------
+
+const getIcon = (name: string) => (
+  <SvgIconStyle src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
+const ICONS = {
+  blog: getIcon('ic_blog'),
+  cart: getIcon('ic_cart'),
+  chat: getIcon('ic_chat'),
+  mail: getIcon('ic_mail'),
+  user: getIcon('ic_user'),
+  kanban: getIcon('ic_kanban'),
+  banking: getIcon('ic_banking'),
+  booking: getIcon('ic_booking'),
+  invoice: getIcon('ic_invoice'),
+  calendar: getIcon('ic_calendar'),
+  ecommerce: getIcon('ic_ecommerce'),
+  analytics: getIcon('ic_analytics'),
+  dashboard: getIcon('ic_dashboard'),
+  menuItem: getIcon('ic_menu_item'),
+};
+
+const navConfig = [
+  {
+    subheader: '',
+    items: [
+      // INVOICE
+      {
+        title: 'Inventory',
+        path: PATH_DASHBOARD.invoice.root,
+        icon: ICONS.invoice,
+        children: [
+          { title: 'Dilutions', path: 'inventory/dilution' },
+          { title: 'Products', path: 'inventory/products' },
+        ],
+      },
+    ],
+  },
+  // MANAGEMENT
+  // ----------------------------------------------------------------------
+  {
+    subheader: '',
+    items: [
+      // INVOICE
+      {
+        title: 'Sales',
+        path: PATH_DASHBOARD.invoice.root,
+        icon: ICONS.invoice,
+        children: [
+          { title: 'list', path: PATH_DASHBOARD.invoice.list },
+          { title: 'details', path: PATH_DASHBOARD.invoice.demoView },
+          { title: 'create', path: PATH_DASHBOARD.invoice.new },
+          { title: 'edit', path: PATH_DASHBOARD.invoice.demoEdit },
+        ],
+      },
+    ],
+  },
+];
+
+export default navConfig;
